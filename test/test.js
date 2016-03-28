45094c6de20c42518db456d03413eab38c9a3de1
Robert Crosby <robmcrosby@gmail.com>
2016-03-28 12:09:27 -0700

    Installed the chai assertion library.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,7 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
-});
+var chai = require('chai');
+var assert = chai.assert;
+
+var foo = 'bar';
+
+assert.typeOf(foo, 'string');
+assert.equal(foo, 'bar');
