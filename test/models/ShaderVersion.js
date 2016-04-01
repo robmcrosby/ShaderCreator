337d0b0988e943a19a8cbcef5473a444f6a49bff
Robert Crosby <robmcrosby@gmail.com>
2016-04-01 15:16:14 -0700

    Added mocha/chai to dev dependencies and revised the unit tests.

--- test/models/ShaderVersion.js
+++ test/models/ShaderVersion.js
@@ -1,79 +1,85 @@
 import ShaderVersion from "../../js/models/ShaderVersion";
 import {expect} from "chai";
 
-// Constructor
-var version = new ShaderVersion('OpenGL 4.1', '#version 410', 'desktop', 4.1);
+describe('ShaderVersion', function() {
+  it('should have expected properties', function() {
+    var version = new ShaderVersion('OpenGL 4.1', '#version 410', 'desktop', 4.1);
 
-// Properties of ShaderVersion
-expect(version).to.have.property('label').to.be.a('string').to.be.equal('OpenGL 4.1');
-expect(version).to.have.property('header').to.be.a('string').to.be.equal('#version 410');
-expect(version).to.have.property('platform').to.be.a('string').to.be.equal('desktop');
-expect(version).to.have.property('number').to.be.a('number').to.be.equal(4.1);
+    expect(version.label).to.equal('OpenGL 4.1');
+    expect(version.header).to.equal('#version 410');
+    expect(version.platform).to.equal('desktop');
+    expect(version.number).to.equal(4.1);
+  });
 
-// Methods of ShaderVersion
-expect(version).to.have.property('vertexInput').to.be.a('function');
-expect(version).to.have.property('vertexOutput').to.be.a('function');
-expect(version).to.have.property('fragmentInput').to.be.a('function');
-expect(version).to.have.property('textureFunction').to.be.a('function');
+  it('should have expected methods', function() {
+    var version = new ShaderVersion('OpenGL 4.1', '#version 410', 'desktop', 4.1);
 
-// Static Methods of ShaderVersion
-expect(ShaderVersion.supportedVersions).to.be.a('function');
+    expect(version.vertexInput).to.be.a('function');
+    expect(version.vertexOutput).to.be.a('function');
+    expect(version.fragmentInput).to.be.a('function');
+    expect(version.textureFunction).to.be.a('function');
+  });
 
-// supportedVersions return value
-var tests = [
-  {label:'OpenGL ES 2.0', header:'#version 100', platform:'embeded', number:2.0},
-  {label:'OpenGL ES 3.0', header:'#version 300 es', platform:'embeded', number:3.0},
-  {label:'OpenGL 4.1', header:'#version 410', platform:'desktop', number:4.1}
-];
-var versions = ShaderVersion.supportedVersions();
-expect(versions).to.have.length(tests.length);
-for (var i = 0; i < tests.legth; ++i) {
-  expect(versions[i].label).is.equal(tests[i].label);
-  expect(versions[i].header).is.equal(tests[i].header);
-  expect(versions[i].platform).is.equal(tests[i].platform);
-  expect(versions[i].number).is.equal(tests[i].number);
-}
+  it('should have supportedVersions return values', function() {
+    expect(ShaderVersion.supportedVersions).to.be.a('function');
+    var versions = ShaderVersion.supportedVersions();
 
-// vertexInput return values
-var tests = [
-  {value:'attribute vec4 test;\n'},
-  {value:'in vec4 test;\n'},
-  {value:'in vec4 test;\n'}
-];
-for (var i = 0; i < tests.length; ++i) {
-  var value = versions[i].vertexInput('vec4', 'test');
-  expect(value).to.be.a('string').to.be.equal(tests[i].value);
-}
+    var expected = [
+      {label:'OpenGL ES 2.0', header:'#version 100', platform:'embeded', number:2.0},
+      {label:'OpenGL ES 3.0', header:'#version 300 es', platform:'embeded', number:3.0},
+      {label:'OpenGL 4.1', header:'#version 410', platform:'desktop', number:4.1}
+    ];
+    expect(versions).to.have.length(expected.length);
 
-// vertexOutput return values
-var tests = [
-  {value:'varying vec4 test;\n'},
-  {value:'out vec4 test;\n'},
-  {value:'out vec4 test;\n'}
-];
-for (var i = 0; i < tests.length; ++i) {
-  var value = versions[i].vertexOutput('vec4', 'test');
-  expect(value).to.be.a('string').to.be.equal(tests[i].value);
-}
+    for (var i = 0; i < versions.legth; ++i) {
+      expect(versions[i].label).is.equal(expected[i].label);
+      expect(versions[i].header).is.equal(expected[i].header);
+      expect(versions[i].platform).is.equal(expected[i].platform);
+      expect(versions[i].number).is.equal(expected[i].number);
+    }
+  });
 
-//fragmentInput(precision, type, name)
-var tests = [
-  {value:'varying mediump vec4 test;\n'},
-  {value:'in mediump vec4 test;\n'},
-  {value:'in vec4 test;\n'}
-];
-for (var i = 0; i < tests.length; ++i) {
-  var value = versions[i].fragmentInput('mediump', 'vec4', 'test');
-  expect(value).to.be.a('string').to.be.equal(tests[i].value);
-}
+  it('should have vertexInput return values', function() {
+    var versions = ShaderVersion.supportedVersions();
+    var expected = [
+      'attribute vec4 test;\n',
+      'in vec4 test;\n',
+      'in vec4 test;\n'
+    ];
+    for (var i = 0; i < versions.length; ++i)
+      expect(versions[i].vertexInput('vec4', 'test')).to.be.equal(expected[i]);
+  });
 
-// textureFunction return values
-var tests = [
-  {value:'texture2D'},
-  {value:'texture'},
-  {value:'texture'}
-];
-for (var i = 0; i < tests.length; ++i) {
-  var value = versions[i].textureFunction();
-  expect(value).to.be.a('string').to.be.equal(tests[i].value);
-}
+  it('should have vertexOutput return values', function() {
+    var versions = ShaderVersion.supportedVersions();
+    var expected = [
+      'varying vec4 test;\n',
+      'out vec4 test;\n',
+      'out vec4 test;\n'
+    ];
+    for (var i = 0; i < versions.length; ++i)
+      expect(versions[i].vertexOutput('vec4', 'test')).to.be.equal(expected[i]);
+  });
+
+  it('should have fragmentInput return values', function() {
+    var versions = ShaderVersion.supportedVersions();
+    var expected = [
+      'varying mediump vec4 test;\n',
+      'in mediump vec4 test;\n',
+      'in vec4 test;\n'
+    ];
+    for (var i = 0; i < versions.length; ++i)
+      expect(versions[i].fragmentInput('mediump', 'vec4', 'test')).to.be.equal(expected[i]);
+  });
+
+  it('should have textureFunction return values', function() {
+    var versions = ShaderVersion.supportedVersions();
+    var expected = [
+      'texture2D',
+      'texture',
+      'texture'
+    ];
+    for (var i = 0; i < versions.length; ++i)
+      expect(versions[i].textureFunction()).to.be.equal(expected[i]);
+  });
+});
